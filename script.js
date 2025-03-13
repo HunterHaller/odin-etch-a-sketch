666da7f345821483d79f68ab513b5c64eb934215
Hunter Haller <huntinghollers@proton.me>
2025-03-13 12:51:51 +0100

    Implemented basic mouseover coloring

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 let defaultGrid = 2
-//let gridContainer = document.querySelector("#container")
 const gridContainer = document.querySelector("#container");
 
 function constructGrid(gridSize){
     let fullSize = gridSize*gridSize
     console.log("Starting construction of grid with size " + fullSize)
-    document.querySelector("#container").style.width = (gridSize*100)+(5*gridSize) + "px";
+    document.querySelector("#container").style.width = (gridSize*100)+(gridSize*2) + "px";
     for (i = 0; i < (gridSize*gridSize); i++){
-        console.log("Constructor looping running iteration " + i)
         const newPixel = document.createElement("div")
         newPixel.classList.add("pixel")
-        newPixel.textContent = "Hello!"
+        newPixel.setAttribute("id", "pixel" + i)
         gridContainer.appendChild(newPixel);
-        
-        console.log("Added a pixel!")
     }
 }
+
+gridContainer.addEventListener("mouseover", (event) => {
+    let target = event.target
+    target.style.backgroundColor = "Black"
+})
+
+
 
 constructGrid(defaultGrid)
